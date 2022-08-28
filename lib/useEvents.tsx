f1f0e187fe82b6b7826b46bbb5723e2e00010949
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2022-08-28 20:01:44 +0200

    Create useEvents hook

---
+++ lib/useEvents.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import useSWR from "swr";
+import EventRow from '../components/EventRow'
+import Event from '../types/Event'
+
+type Options = {
+    q: string
+}
+
+type PageProps = {
+    index: number,
+    options: Options,
+    onLoading?: (loading: boolean) => any,
+    onError?: (error: boolean) => any,
+    onHasMore?: (hasMore: boolean) => void,
+}
+
+type EventResponse = {
+    events: Event[],
+    hasMore: boolean
+}
+
+export default function useEvents(options: Options) {
+    const [index, setIndex] = useState(1)
+    const pages = []
+    const loadMore = () => setIndex(index + 1)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+    const [hasMore, setHasMore] = useState(false)
+
+    for (let i = 1; i < index; i++) {
+        pages.push(<Page index={i} options={options} />)
+    }
+
+    pages.push(<Page index={index} options={options} onLoading={setLoading} onError={setError} onHasMore={setHasMore} />)
+
+    return {pages, loading, error, hasMore, loadMore}
+    
+}
+
+function Page ({ index, options, onLoading, onError, onHasMore } : PageProps ) {
+    const fetcher = (input: RequestInfo | URL, init?: RequestInit | undefined) => fetch(input, init).then(res => res.json())
+    let url = `/api/events?page=${index}`
+    if (options.q) url += `&q=${options.q}`
+    const { data, error } = useSWR<EventResponse>(url, fetcher);
+  
+    if (onError) onError(!!error)
+    if (onLoading) onLoading(!data)
+    if (error || !data) return <></>;
+    if (onHasMore) onHasMore(data.hasMore)
+    return <>{data.events.map(event => <EventRow event={event} key={event.id} />)}</>
+}
