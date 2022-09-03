1666c3d7add11ee3dc9c16b14d00b06cec32fcc8
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2022-09-03 15:25:07 +0200

    Add Action Name to filters

--- components/FilterButton.tsx
+++ components/FilterButton.tsx
@@ -3,15 +3,17 @@ import { useState } from "react"
 
 export default function FilterButton({ onFilter } : { onFilter: (filter: string) => void }) {
     const [toggle, setToggle] = useState(false)
-    const [actor, setActor] = useState('')
-    const [target, setTarget] = useState('')
-    const [action, setAction] = useState('')
+    const [actorId, setActorId] = useState('')
+    const [targetId, setTargetId] = useState('')
+    const [actionId, setActionId] = useState('')
+    const [actionName, setActionName] = useState('')
 
     function handleFilter() {
         let filter = ''
-        if (actor) filter += `&actor_id=${actor}`
-        if (target) filter += `&target_id=${action}`
-        if (action) filter += `&action_id=${action}`
+        if (actorId) filter += `&actor_id=${actorId}`
+        if (targetId) filter += `&target_id=${actionId}`
+        if (actionId) filter += `&action_id=${actionId}`
+        if (actionName) filter += `&action_name=${actionName}`
         onFilter(filter)
     }
 
@@ -27,15 +29,19 @@ export default function FilterButton({ onFilter } : { onFilter: (filter: string)
                     <div className="absolute top-[50px] right-0 w-[400px] bg-white rounded border p-3">
                         <div className="flex items-center pb-2">
                             <div className="flex-1">Actor ID</div>
-                            <input className="flex-[2] border p-1" value={actor} onChange={e => setActor(e.target.value)}/>
+                            <input className="flex-[2] border p-1" value={actorId} onChange={e => setActorId(e.target.value)}/>
                         </div>
                         <div className="flex items-center pb-2">
                             <div className="flex-1">Target ID</div>
-                            <input className="flex-[2] border p-1" value={target} onChange={e => setTarget(e.target.value)} />
+                            <input className="flex-[2] border p-1" value={targetId} onChange={e => setTargetId(e.target.value)} />
                         </div>
                         <div className="flex items-center pb-2">
                             <div className="flex-1">Action ID</div>
-                            <input className="flex-[2] border p-1" value={action} onChange={e => setAction(e.target.value)} />
+                            <input className="flex-[2] border p-1" value={actionId} onChange={e => setActionId(e.target.value)} />
+                        </div>
+                        <div className="flex items-center pb-2">
+                            <div className="flex-1">Action Name</div>
+                            <input className="flex-[2] border p-1" value={actionName} onChange={e => setActionName(e.target.value)} />
                         </div>
                         <button className="w-full bg-[#F5F5F5] font-['Inter'] font-semibold text-[14px] text-[#616161] tracking-[0.02em] uppercase rounded p-2" onClick={handleFilter}>Filter</button>
                     </div>
