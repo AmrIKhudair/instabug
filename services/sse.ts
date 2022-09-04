4e71334f2890aaa82bf350d8476aedc3e7fb8f69
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2022-09-04 06:00:58 +0200

    Create an SSE service

--- services/sse.ts
+++ services/sse.ts
@@ -1,4 +1,17 @@
-import { createChannel } from "better-sse"
+import { createChannel, createSession, Session } from "better-sse"
+import { NextApiRequest, NextApiResponse } from "next"
 
-const channel = createChannel()
-export default channel
+const sessions: { req: NextApiRequest, session: Session }[] = []
+
+export async function session(req: NextApiRequest, res: NextApiResponse) {
+    const session = await createSession(req, res)
+    session.on('disconnected', () => sessions.splice(sessions.findIndex(entry => entry.session === session), 1))
+    sessions.push({ req, session })
+}
+
+export function broadcast(handler: (req: NextApiRequest) => any, eventName = 'message') {
+    sessions.forEach(async ({ req, session }) => {
+        const data = await handler(req)
+        if (data) session.push(data, eventName)
+    })
+}
