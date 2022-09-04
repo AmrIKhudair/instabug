c1437d5a489be64c87e2378847a5306eee014cf7
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2022-09-04 13:56:31 +0200

    Improve the filter form

--- components/FilterButton.tsx
+++ components/FilterButton.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 
 export default function FilterButton({ onFilter } : { onFilter: (filter: string) => void }) {
     const [toggle, setToggle] = useState(false)
@@ -8,13 +8,15 @@ export default function FilterButton({ onFilter } : { onFilter: (filter: string)
     const [actionId, setActionId] = useState('')
     const [actionName, setActionName] = useState('')
 
-    function handleFilter() {
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
         let filter = ''
         if (actorId) filter += `&actor_id=${actorId}`
         if (targetId) filter += `&target_id=${targetId}`
         if (actionId) filter += `&action_id=${actionId}`
         if (actionName) filter += `&action_name=${actionName}`
         onFilter(filter)
+        setToggle(false)
     }
 
     return (
@@ -24,7 +26,7 @@ export default function FilterButton({ onFilter } : { onFilter: (filter: string)
             </button>
 
             { toggle && (
-                <>
+                <form onSubmit={handleSubmit}>
                     <div className="fixed w-full h-full left-0 top-0" onClick={() => setToggle(false)} />
                     <div className="absolute top-[50px] right-0 w-[400px] bg-white rounded border p-3">
                         <div className="flex items-center pb-2">
@@ -43,9 +45,9 @@ export default function FilterButton({ onFilter } : { onFilter: (filter: string)
                             <div className="flex-1">Action Name</div>
                             <input className="flex-[2] border p-1" value={actionName} onChange={e => setActionName(e.target.value)} />
                         </div>
-                        <button className="w-full bg-[#F5F5F5] font-['Inter'] font-semibold text-[14px] text-[#616161] tracking-[0.02em] uppercase rounded p-2" onClick={handleFilter}>Filter</button>
+                        <button type="submit" className="w-full bg-[#F5F5F5] font-['Inter'] font-semibold text-[14px] text-[#616161] tracking-[0.02em] uppercase rounded p-2">Filter</button>
                     </div>
-                </>
+                </form>
             )}
         </div>
     )
